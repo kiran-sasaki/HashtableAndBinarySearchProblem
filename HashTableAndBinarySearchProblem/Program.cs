a66f483759b4d3c620f82c7916d7217412b12019
kiran-sasaki <kiransekar1111@gmail.com>
2022-11-30 15:39:15 +0530

    Added Welcome Message For HashtableAndBinarySearchTree Problem

---
+++ HashTableAndBinarySearchProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HashTableAndBinarySearchProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Hash Table and Binary Search Tree Problem");
+        }
+    }
+}
