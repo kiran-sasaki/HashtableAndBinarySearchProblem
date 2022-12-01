8552d61e8856a8a818b002e106238b36526dba3d
kiran-sasaki <kiransekar1111@gmail.com>
2022-12-01 16:00:32 +0530

    Added BinarySearchTree Operation Functions For HashtableAndBinarySearchTree Problem

---
+++ HashTableAndBinarySearchProblem/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableAndBinarySearchProblem
+{
+    public class Node
+    {
+        public Node RightNode { get; set; }
+        public Node LeftNode { get; set; }
+        public int data { get; set; }
+    }
+}
